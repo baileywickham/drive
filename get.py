3844f6beba1b22594972cd7d4f3a07fafeee7af1
baileywickham <baileywickham@gmail.com>
2018-05-10 13:23:48 -0700

    added meta file, got search working`:`

--- get.py
+++ get.py
@@ -6,8 +6,21 @@ gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
-fileList = drive.ListFile({'q' : 'type=document'}).GetList()
+fileList = drive.ListFile({'q' : "mimeType = 'application/vnd.google-apps.document'"}).GetList()
+
+docs = {}
+
 for i in fileList:
+    docs.update({ i['title'] : i['id']})
     # str literal, needs > 3.6
     # print(f"the name is {f.title}")
-    print('title: %s, id: %s' % (i['title'], i['id']))
+    # print('title: %s, id: %s, type: %s'  % (i['title'], i['id'], i['mimeType']))
+
+
+
+# Should docs be global or should I pass it in?
+def downloadDocument(name, id):
+    fileToWrite = drive.CreateFile({name : id})
+    fileToWrite.GetContentFile(name)
+    with open('Downloaded/' + name, 'w') as localFile:
+        localFile.write(fileToWrite)
