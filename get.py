317c41be714b40837378b92187407a6f2bf6091e
baileywickham <baileywickham@gmail.com>
2018-05-15 13:30:51 -0700

    things still don't work but they are getting better

--- get.py
+++ get.py
@@ -10,27 +10,24 @@ drive = GoogleDrive(gauth)
 def getList():
     # sharedWithMe = false returns a 404 error. This is an api problem.
     fileList = drive.ListFile({'q' : "mimeType = 'application/vnd.google-apps.document'", 'maxResults' : 10}).GetList()
-    
+
     #No idea when this would fail but it looks good.
     assert fileList is not None
 
     # str literal, needs > 3.6
     # print(f"the name is {f.title}")
-    # listings.outputList([x['title'] for x in fileList]) 
     # passes full list for later metadata support
     request = listings.outputList(fileList)
-    print(type(fileList))
-    idRequests = [ fileList['title'].get(x) for x in request.values()]
- 
+    downloadDocument(request=request) 
 
 # force keyword args out of laziness
-def downloadDocument(*, request, fileList):
-    fileToWrite = drive.CreateFile()
-    fileToWrite.GetContentFile()
-    with open('Downloaded/', 'w') as localFile:
-        localFile.write(fileToWrite)
+def downloadDocument(*, request):
+    for id in request:
+        fileToWrite = drive.CreateFile({'id' : id})
+        fileToWrite.FetchMetadata()
+        fileToWrite.GetContentFile('./Downloaded/' + fileToWrite['title'])
 
-
+# is this even good practice any more? Is there a better standard? Probably.
 def main():
     getList()
 
--- listings.py
+++ listings.py
@@ -1,11 +1,8 @@
 # I think this will be depricated.
 import os
 import xattr
-import readchar
 from inquirer import Checkbox, prompt
 
-# there has to be a better way to do this.
-# turns out there is and it has half the number of lines.
 def getDownloadedFiles():
     idList = [xattr.getxattr(file, 'user.docid') for file in os.listdir('./Downloaded')] 
     # generator because it should only be compared once. 
@@ -17,5 +14,5 @@ def outputList(fileList):
     c = [Checkbox('docs', message='List of Google Docs:', choices=[x['title'] for x in fileList]) ]
     # need to impliment already downloaded check, then return a clean
     # prompt list
-    return prompt(c)
+    return list(prompt(c).values())
     
